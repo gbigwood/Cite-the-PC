d1533e9bcd0b6ed32feff53656df2c148efc6024
gjb <theshadowhost@gmail.com>
2011-09-10 17:51:45 +0100

    working parsing of the different types so far

--- citeTPC.py
+++ citeTPC.py
@@ -14,57 +14,64 @@ from BeautifulSoup import BeautifulSoup
 from collections import namedtuple
 import urllib2
 #The hardcoded strings for tags
-paperContainer = "gs_r"
-linkToPaper = "gs_ggs"
+paperContainerString = "gs_r"
+linkToPaperString = "gs_ggs"
+citationString = "gs_ctu"
+#Create opener with Google-friendly user agent
+opener = urllib2.build_opener()
+opener.addheaders = [('User-agent', 'Mozilla/5.0')]
 
 #Make a container type for the pc members
 Paper = namedtuple('Paper', ['doi', 'hardlink'])
 
 #Who we are searching for
 listofnames = [
-    "Kevin Almeroth University of California",
-    "Sun-Ki Chai University of Hawaii",
-    "Adrian David Cheok National University of Singapore",
-    "Noshir Contractor Northwestern University",
-    "Irfan Essa Georgia Institute of Technology",
-    "David Lazer Northeastern/Harvard University",
+    #"Kevin Almeroth University of California",
+    #"Sun-Ki Chai University of Hawaii",
+    #"Adrian David Cheok National University of Singapore",
+    #"Noshir Contractor Northwestern University",
+    #"Irfan Essa Georgia Institute of Technology",
+    #"David Lazer Northeastern/Harvard University",
     "Dongman Lee KAIST",
     "Ramesh Jain University of California",
-    "Tom Malone Massachusetts Institute of Technology",
-    "Kenji Mase Nagoya University",
+    #"Tom Malone Massachusetts Institute of Technology",
+    #"Kenji Mase Nagoya University",
 ]
 
 def searchForTPC(members):
     """takes a list of members, returns their papers"""
     for member in members:
+        print "Member:",member
         member = member.replace(" ","+")
-        print member
         for paper in findMembersPapers(member):
             try:
-                for link in paper:
-                    print link
+                print paper.doi
+                print paper.hardlink
+                print "-"
             except:
                 print "no links"
         print "---"
+        print "---"
 
-#Create opener with Google-friendly user agent
 def findMembersPapers(memberString):
     """takes a member to search for, returns a list of html urls.
     
     the urls come as """
-    opener = urllib2.build_opener()
-    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
     url = "http://scholar.google.com/scholar?q="+memberString
     page = opener.open(url)
     soup = BeautifulSoup(page)
     results = [] #the result papers go in here
     #print soup.prettify()
     #Parse and find the gs_r tags
-    for cite in soup.findAll(name='div',attrs={"class" : paperContainer}):
-        if linkToPaper in cite.span['class']: # contains a link to the hardcopy
-            p = (cite.div.a, cite.span.a)
-        else:
-            p = (cite.div.a)
+    for cite in soup.findAll(name='div',attrs={"class" : paperContainerString}):
+        #pass
+        if linkToPaperString in cite.span['class']: # contains a link to the hardcopy
+            p = Paper(cite.div.a, cite.span.a)#we know there will be two links with data
+        elif citationString in cite.span['class']: #its a citation not a paper
+            #print "skipping",cite.div
+            continue
+        else:#no paper link
+            p = Paper(cite.div.a,None)
         results.append(p)
     return results
 
