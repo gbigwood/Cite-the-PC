c6b7e0c55effa5db1da0cd9aafde354b8c109c96
gjb <theshadowhost@gmail.com>
2011-09-10 13:11:54 +0100

    added in the new file

---
+++ citeTPC.py
@@ -0,0 +1,13 @@
+"""
+A program to obtain a list of all the papers that you need to cite to get 
+accepted at your conference.             
+ __                 
+/  \        _____________ 
+|  |       /             \
+@  @       | It looks    |
+|| ||      | like you    |
+|| ||   <--| are writing |
+|\_/|      | a paper.    |
+\___/      \_____________/
+             
+"""
