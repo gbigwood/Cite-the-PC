8b762892b595b8893b05e0a64f89201ddcf55a1d
gjb <theshadowhost@gmail.com>
2011-09-10 13:28:47 +0100

    import and listofnames

--- citeTPC.py
+++ citeTPC.py
@@ -1,13 +1,31 @@
 """
-A program to obtain a list of all the papers that you need to cite to get 
-accepted at your conference.             
- __                 
-/  \        _____________ 
+A program to obtain a list of all the papers that you need to cite to get
+accepted at your conference.
+ __
+/  \        _____________
 |  |       /             \
 @  @       | It looks    |
 || ||      | like you    |
 || ||   <--| are writing |
 |\_/|      | a paper.    |
 \___/      \_____________/
-             
 """
+from BeautifulSoup import BeautifulSoup
+
+#Make a container type for the pc members
+Member = namedtuple('PCMember', ['name', 'institution'], verbose=True)
+
+listofnames = [
+Member("Kevin Almeroth", "University of California"),
+Member("Sun-Ki Chai", "University of Hawaii"),
+Member("Adrian David Cheok", "National University of Singapore"),
+Member("Noshir Contractor", "Northwestern University"),
+Member("Irfan Essa", "Georgia Institute of Technology"),
+Member("David Lazer", "Northeastern/Harvard University"),
+Member("Dongman Lee", "KAIST"),
+Member("Ramesh Jain", "University of California"),
+Member("Tom Malone", "Massachusetts Institute of Technology"),
+Member("Kenji Mase", "Nagoya University"),
+]
+
+
