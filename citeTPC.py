9d2993fa2f5016f374ec8e86b5c73d160706d259
gjb <theshadowhost@gmail.com>
2011-09-10 13:37:23 +0100

    get a working example

--- citeTPC.py
+++ citeTPC.py
@@ -12,21 +12,36 @@ accepted at your conference.
 """
 from BeautifulSoup import BeautifulSoup
 from collections import namedtuple
+import urllib2
+
 
 #Make a container type for the pc members
 Member = namedtuple('PCMember', ['name', 'institution'])
 
+#who we are searching for
 listofnames = [
-Member("Kevin Almeroth", "University of California"),
-Member("Sun-Ki Chai", "University of Hawaii"),
-Member("Adrian David Cheok", "National University of Singapore"),
-Member("Noshir Contractor", "Northwestern University"),
-Member("Irfan Essa", "Georgia Institute of Technology"),
-Member("David Lazer", "Northeastern/Harvard University"),
-Member("Dongman Lee", "KAIST"),
-Member("Ramesh Jain", "University of California"),
-Member("Tom Malone", "Massachusetts Institute of Technology"),
-Member("Kenji Mase", "Nagoya University"),
+    Member("Kevin Almeroth", "University of California"),
+    Member("Sun-Ki Chai", "University of Hawaii"),
+    Member("Adrian David Cheok", "National University of Singapore"),
+    Member("Noshir Contractor", "Northwestern University"),
+    Member("Irfan Essa", "Georgia Institute of Technology"),
+    Member("David Lazer", "Northeastern/Harvard University"),
+    Member("Dongman Lee", "KAIST"),
+    Member("Ramesh Jain", "University of California"),
+    Member("Tom Malone", "Massachusetts Institute of Technology"),
+    Member("Kenji Mase", "Nagoya University"),
 ]
 
+### Create opener with Google-friendly user agent
+opener = urllib2.build_opener()
+opener.addheaders = [('User-agent', 'Mozilla/5.0')]
 
+url = "http://http://scholar.google.com/scholar?q=Kevin+Almeroth+University+of+California"
+page = opener.open(url)
+soup = BeautifulSoup(page)
+
+### Parse and find
+### Looks like google contains URLs in <cite> tags.
+### So for each cite tag on each page (10), print its contents (url)
+for cite in soup.findAll('gs_rt'):
+    print cite.text
