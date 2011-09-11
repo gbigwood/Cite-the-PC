9f0b0a368f90fa48035bbc60b5339f61c53076db
gjb <theshadowhost@gmail.com>
2011-09-11 16:42:18 +0100

    cleaned up css.

--- webversion.py
+++ webversion.py
@@ -24,24 +24,25 @@ class LandingPage:
         outputtext.append('</body></html>')
         return outputtext#cherrypy will join items for us
 
-    def getHeaderText(self):
+    def getHeaderText(self,title="Cite The PC"):
         """gets the page header"""
         #TODO take an optional arguement for the title
         return """
-        <html><head><title>Cite The PC</title>
+        <html><head><title>%s</title>
         <link rel="stylesheet" type="text/css" href="/css/citeTPC.css" media="screen" />
-        </head><body>"""
+        </head><body>""" % title
 
     def createOpeningMessage(self):
         """
         creates the welcome message
         """
         return """
-        <div class="welcome"><h1>Welcome to Cite The PC</h1>
+        <div class="welcome">
+        <h1>Welcome to Cite The PC</h1>
         <p>Ever tried to get a paper published only to be told to reference papers you know were written by the programme committee? 
         Need to suck up to get a paper accepted in your field?
         </p>
-        <p>Now with cite the PC its never been easier to cite the programme committee! Simply enter the names of the committee into the box below, to get a returned list of the committee member's papers (with links).
+        <p>Now with Cite The PC, it's never been easier to cite the programme committee! Simply enter the names of the committee into the box below, to get a returned list of the committee member's papers (with links).
         </p>
         </div>
         """
@@ -62,13 +63,13 @@ class LandingPage:
     Kevin Almeroth University of California</br>
     Sun-Ki Chai University of Hawaii</br>
     Adrian David Cheok National University of Singapore</br>
-    Noshir Contractor Northwestern University</br>
+    Dongman Lee KAIST</br>
     </div>
     </p>
     </div>
     <div class="submitform">
-    <form action="submitMembers" method="post" >
-        <p>Enter the members below:</p>
+    <form action="results" method="post" >
+        <p><h3>Enter the PC members below:</h3></p>
         <textarea cols="60" rows="20" name="membernames"></textarea>
         <p><input type="submit" value="Find Papers"/></p>
     </form>
@@ -76,19 +77,20 @@ class LandingPage:
     
     """    
 
-    def submitMembers(self, membernames = None):
+    def results(self, membernames = None):
         """takes the members passed in from the form and calls the citeTPC code
         """
         #handle the newline character
         membernames = membernames.split(unichr(13))
         membernames = (member.strip("\n") for member in membernames)
         outputtext = []
-        outputtext.append(self.getHeaderText())
+        outputtext.append(self.getHeaderText(title="Results"))
+        outputtext.append("""<h1>Papers to cite</h1>""")
         outputtext.append("\n".join(citeTPC.searchForTPC(membernames)))
         outputtext.append('</body></html>')
         return outputtext
 
-    submitMembers.exposed = True
+    results.exposed = True
     index.exposed = True
 
 
