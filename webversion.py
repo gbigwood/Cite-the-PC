ba920c2ed456826b72fd7ae63124e9cb3b020944
gjb <theshadowhost@gmail.com>
2011-09-11 14:27:04 +0100

    updated the website and css so that it serves static content correctly

--- webversion.py
+++ webversion.py
@@ -14,7 +14,7 @@ class LandingPage:
         join a header and a body and a footer together
         """
         header = """<html><head><title>Papers to cite</title>
-        <link rel="stylesheet" type="text/css" href="sitefiles/citeTPC.css" media="screen" />
+        <link rel="stylesheet" type="text/css" href="/css/citeTPC.css" media="screen" />
         </head><body>"""
         body = self.createOpeningMessage()
         footer = '</body></html>'
@@ -23,21 +23,26 @@ class LandingPage:
         """
         creates the welcome message
         """
-        welcomestring = """
+        return """
         <div class="welcome"><h1>Welcome to citeTPC</h1>
         <p>Ever tried to get a paper published only to be told to reference papers you know were written by the programme committee? 
         Need to suck up to get a paper accepted in your field?
         </p>
         <p>Now with citeTPC its never been easier to cite the programme committee! Simply enter the names of the committee into the box below, to get a returned list of the committee member's papers.
         </p>
+        </div>
         """
-        return welcomestring
 
     index.exposed = True
-# CherryPy always starts with cherrypy.root when trying to map request URIs
-# to objects, so we need to mount a request handler object here. A request
-# to '/' will be mapped to cherrypy.root.index().
-cherrypy.root = LandingPage()
 
 if __name__ == '__main__':
-    cherrypy.quickstart(LandingPage())
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    # Set up site-wide config first so we get a log if errors occur.
+    cherrypy.config.update({'environment': 'production',
+                            'log.error_file': 'site.log',
+                            'log.screen': True})
+        
+    conf = {'/css': {'tools.staticdir.on': True,
+                      'tools.staticdir.dir': os.path.join(current_dir, 'data/css'),
+                      }}
+    cherrypy.quickstart(LandingPage(), '/', config=conf)
