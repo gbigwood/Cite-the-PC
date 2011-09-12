63ba081d0967d1869a45bf196c9cd76badb85519
gjb <theshadowhost@gmail.com>
2011-09-12 10:26:16 +0100

    trying to output the member name as correct

--- citeTPC.py
+++ citeTPC.py
@@ -75,9 +75,8 @@ def searchForTPC(members):
     outputstrings = []
     for member in members:
         outputstrings.append('<div class="member">')
-        #outputstrings.append('<div class="membername">%s</div>' 
-        #%member.decode('latin-1'))
-        outputstrings.append('<div class="membername">%s</div>' % member)
+        outputstrings.append('<div class="membername">%s</div>' %member.decode('latin-1'))
+        #outputstrings.append('<div class="membername">%s</div>' % member)
         member = formatMemberText(member)
         outputstrings.append('<ul>')
         for paper in findMembersPapers(member):
